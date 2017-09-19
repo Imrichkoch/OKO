d4783e554414e99a8ab645ccc149a5a8014ac728
Imrichkoch <imrichkoch@gmail.com>
2017-09-19 19:20:57 +0200

    .

--- OKO10/src/model/Card.java
+++ OKO10/src/model/Card.java
@@ -1,32 +1,33 @@
 package model;
 
 public class Card {
-	
+
 	private Color color;
 	private Value value;
-	
-	public Card(Color color,Value value){
+
+	public Card(Color color, Value value) {
 		this.color = color;
 		this.value = value;
 	}
-	
-	public Card(){
-		
+
+	public Card() {
+
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
+
 	public Value getValue() {
 		return value;
 	}
+
 	public void setValue(Value value) {
 		this.value = value;
 	}
-	
-	
 
 }
